56f36b8000e43af1ed6c371fa2715e3f558a655b
Yuan <apexy2007@gmail.com>
2013-03-17 20:04:44 +0800

    finish layout and routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,25 +1,33 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject {page}
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
+    before { visit root_path }
+
+    it {should have_selector('h1', text:'Sample App')}
   end
 
   describe "Help page" do
     it "should have the h1 content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', text:'Help')
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
   end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text:'Contact')
+    end
+  end
+
 end
